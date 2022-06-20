434f05597da01d972aef4edaebdb902a47f78ca3
James Hager <james@avantiapplications.com>
2022-06-20 13:56:59 -0600

    Initial Commit

---
+++ CreateUserTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CreateUserTest
+//
+//  Created by James Hager on 6/20/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
